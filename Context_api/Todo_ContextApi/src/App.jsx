38f7760951eb8236fb625e5833693a9f860895a5
Ansh Singh <anshsingh2529@gmail.com>
2024-01-04 18:19:57 +0000

    some

--- Context_api/Todo_ContextApi/src/App.jsx
+++ Context_api/Todo_ContextApi/src/App.jsx
@@ -1,11 +1,32 @@
+import { TodoContextProvider } from "./contexts"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Todos, setTodo] = useState([]);
 
+  const addTodo = (todo) => {
+    
+  }
   return (
-    <>
-     <h1>ansh using Context APi and Making Todo with React</h1>
-    </>
+    <TodoContextProvider value={{Todos, addTodo, updateTodo, deleteTodo, toggleCompleted}}>
+    <div>
+      <div className=" bg-[#172842] flex justify-center pt-20 text-6xl font-bold pb-10" >
+      Context APi &  Todo App with React
+      </div>
+
+    <div className="bg-[#172842] min-h-screen py-8">
+      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+              {/* Todo form goes here */} 
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+              {/*Loop and Add TodoItem here */}
+          </div>
+      </div>
+    </div>
+</div>
+</ TodoContextProvider>
+    
   )
 }
 
